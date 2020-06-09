6b5bc8157a87c5c6a44d6d12a5dc1de56acafe2f
devcrp <devcrp>
2020-06-09 23:34:02 +0200

    JumperMovementSpecification progress

--- src/api/ChessGame.Domain.Tests/Specifications/JumperMovementSpecificationTests.cs
+++ src/api/ChessGame.Domain.Tests/Specifications/JumperMovementSpecificationTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using ChessGame.Domain.Entities;
+using ChessGame.Domain.Specifications.Movements;
+using ChessGame.Domain.ValueObjects;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,13 +11,41 @@ namespace ChessGame.Domain.Tests.Specifications
     public class JumperMovementSpecificationTests
     {
         [Test]
-        public void Jumper_Movement_Should_Satisfy()
+        public void Straight_Jumper_Movement_Should_Satisfy()
+        {
+            Board board = Board.CreateAndSetup();
+
+            JumperMovementSpecification jumperMovementSpecification = JumperMovementSpecification.Create(board);
+
+            PieceMovement pieceMovement = PieceMovement.Create(board.GetSquare("A1").Piece, 
+                                                               Position.Create("A1"), 
+                                                               Position.Create("A5"));
+
+            Assert.IsTrue(jumperMovementSpecification.IsSatisfied(pieceMovement));
+        }
+
+        [Test]
+        public void Straight_Not_Jumper_Movement_Should_Not_Satisfy()
+        {
+            Board board = Board.CreateAndSetup();
+
+            JumperMovementSpecification jumperMovementSpecification = JumperMovementSpecification.Create(board);
+
+            PieceMovement pieceMovement = PieceMovement.Create(board.GetSquare("A2").Piece,
+                                                               Position.Create("A2"),
+                                                               Position.Create("A4"));
+
+            Assert.IsFalse(jumperMovementSpecification.IsSatisfied(pieceMovement));
+        }
+
+        [Test]
+        public void Diagonal_Jumper_Movement_Should_Satisfy()
         {
             Assert.Fail();
         }
 
         [Test]
-        public void Not_Jumper_Movement_Should_Not_Satisfy()
+        public void Diagonal_Not_Jumper_Movement_Should_Not_Satisfy()
         {
             Assert.Fail();
         }
--- src/api/ChessGame.Domain/Specifications/Movements/JumperMovementSpecification.cs
+++ src/api/ChessGame.Domain/Specifications/Movements/JumperMovementSpecification.cs
@@ -1,15 +1,55 @@
-﻿using ChessGame.Domain.ValueObjects;
+﻿using ChessGame.Domain.Entities;
+using ChessGame.Domain.ValueObjects;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ChessGame.Domain.Specifications.Movements
 {
     public class JumperMovementSpecification : ISpecification<PieceMovement>
     {
+        private readonly Board _board;
+
+        public static JumperMovementSpecification Create(Board board) => new JumperMovementSpecification(board);
+
+        private JumperMovementSpecification(Board board)
+        {
+            this._board = board;
+        }
+
         public bool IsSatisfied(PieceMovement candidate)
         {
-            throw new NotImplementedException();
+            if (candidate.From.FileIndex == candidate.To.FileIndex
+                || candidate.From.RankIndex == candidate.To.RankIndex)
+            {
+                return StraightMovementIsSatisfied(candidate);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private bool StraightMovementIsSatisfied(PieceMovement candidate)
+        {
+            bool isJumper;
+            if (candidate.From.FileIndex == candidate.To.FileIndex)
+            {
+                isJumper = _board.Squares.Where(square => square.Position.FileIndex == candidate.From.FileIndex
+                                               && square.Position.RankIndex > candidate.From.RankIndex
+                                               && square.Position.RankIndex < candidate.To.RankIndex)
+                                         .Any(square => !square.IsEmpty);
+            }
+            else
+            {
+                isJumper = _board.Squares.Where(square => square.Position.RankIndex == candidate.From.RankIndex
+                                               && square.Position.FileIndex > candidate.From.FileIndex
+                                               && square.Position.FileIndex < candidate.To.FileIndex)
+                                         .Any(square => !square.IsEmpty);
+            }
+
+            return isJumper;
         }
     }
 }
